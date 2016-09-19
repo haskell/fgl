166cb516f5448d20f19cc3d46332ce7604fecc88
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2016-09-19 11:56:57 +1000

    Adjancy related functions

--- Data/Graph/Inductive/New/Graph.hs
+++ Data/Graph/Inductive/New/Graph.hs
@@ -1,4 +1,6 @@
-{-# LANGUAGE FlexibleContexts, TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts, StandaloneDeriving, TypeFamilies,
+             UndecidableInstances #-}
+
 {- |
    Module      : Data.Graph.Inductive.New.Graph
    Description : Graph data structure representations
@@ -42,6 +44,29 @@ class (Eq (Vertex g), Eq (Edge g)) => Graph g where
 
   hasEdge :: g -> Edge g -> Bool
   hasEdge g e = e `elem` edges g
+
+  inverseEdge :: g -> Edge g -> Maybe (Edge g)
+
+  adjacentTo :: g -> Edge g -> Maybe (Vertex g)
+
+  adjNeighbours :: g -> Vertex g -> Maybe [Adjacency g]
+  -- TOOD: write a default using adjacent, inverseEdge and adjacentTo
+
+  adjacent :: g -> Vertex g -> Maybe [Edge g]
+  adjacent g v = map adjEdge <$> (adjNeighbours g v)
+
+  neighbours :: g -> Vertex g -> Maybe [Vertex g]
+  neighbours g v = map neighbour <$> (adjNeighbours g v)
+
+data Adjacency g = Adj { adjEdge   :: Edge g
+                       , invEdge   :: Edge g
+                       , neighbour :: Vertex g
+                       }
+
+deriving instance (Eq (Vertex g), Eq (Edge g)) => Eq (Adjacency g)
+deriving instance (Ord (Vertex g), Ord (Edge g)) => Ord (Adjacency g)
+deriving instance (Show (Vertex g), Show (Edge g)) => Show (Adjacency g)
+deriving instance (Read (Vertex g), Read (Edge g)) => Read (Adjacency g)
 
 --------------------------------------------------------------------------------
 
@@ -90,3 +115,26 @@ instance Graph (Gr a b) where
   hasVertex g v = v `M.member` grVertices g
 
   hasEdge g e = e `M.member` grEdges g
+
+  inverseEdge g e = grEdgInv <$> M.lookup e (grEdges g)
+
+  adjacentTo g e = grEdgVer <$> M.lookup e (grEdges g)
+
+  adjNeighbours g v = map (mkAdj g) <$> adjacent g v
+
+  adjacent g v = grAdj <$> (v `M.lookup` grVertices g)
+
+  neighbours g v = map (snd . grAdjNbr g) <$> adjacent g v
+
+mkAdj :: Gr a b -> GrEdge -> Adjacency (Gr a b)
+mkAdj g e = let (e', nbr) = grAdjNbr g e
+            in Adj e e' nbr
+
+grAdjNbr :: Gr a b -> GrEdge -> (GrEdge, GrVertex)
+grAdjNbr g e = (e', nbr)
+  where
+    getEInfo = (grEdges g M.!)
+
+    e' = grEdgInv (getEInfo e)
+
+    nbr = grEdgVer (getEInfo e')
